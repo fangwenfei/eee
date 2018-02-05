56db4624150519405e6107cf15de770d281fac6e
闫鹏磊 <75997533@qq.com>
2018-02-05 16:11:49 +0800

    Spring Boot 中使用 Java API 调用 Elasticsearch

--- spring-boot-elasticsearch-demo/src/main/java/io/ymq/example/elasticsearch/config/ElasticsearchConfig.java
+++ spring-boot-elasticsearch-demo/src/main/java/io/ymq/example/elasticsearch/config/ElasticsearchConfig.java
@@ -10,7 +10,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
-
 import java.net.InetAddress;
 
 /**
