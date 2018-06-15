eded3cd844a3586640e41a3595006cd34698395e
搜云库 <admin@souyunku.com>
2018-06-16 00:38:17 +0800

    Update LogAspect.java
    去掉 try  catch

--- spring-boot-logback/src/main/java/io/ymq/logback/config/commons/LogAspect.java
+++ spring-boot-logback/src/main/java/io/ymq/logback/config/commons/LogAspect.java
@@ -33,56 +33,43 @@ public class LogAspect {
     public void serviceLog() {
     }
 
-    @Around("serviceLog()")
-    public Object around(ProceedingJoinPoint joinPoint) {
-        try {
+   @Around("serviceLog()")
+    public Object around(ProceedingJoinPoint joinPoint) throws Throwable {
+        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+        Method method = signature.getMethod();
+        Class<?> targetClass = method.getDeclaringClass();
 
-            String requestUUID = MDC.get("requestUUID");
-            if (requestUUID == null || "".equals(requestUUID)) {
-                String uuid = UUID.randomUUID().toString();
-                uuid = uuid.replaceAll("-", "").toUpperCase();
-                MDC.put("requestUUID", uuid);
-                log.info("around 在请求处理之前生成 logback requestUUID:{}", uuid);
+        StringBuffer classAndMethod = new StringBuffer();
+
+        Log classAnnotation = targetClass.getAnnotation(Log.class);
+        Log methodAnnotation = method.getAnnotation(Log.class);
+
+        if (classAnnotation != null) {
+            if (classAnnotation.ignore()) {
+                return joinPoint.proceed();
             }
-
-            MethodSignature signature = (MethodSignature) joinPoint.getSignature();
-            Method method = signature.getMethod();
-            Class<?> targetClass = method.getDeclaringClass();
-
-            StringBuffer classAndMethod = new StringBuffer();
-
-            Log classAnnotation = targetClass.getAnnotation(Log.class);
-            Log methodAnnotation = method.getAnnotation(Log.class);
-
-            if (classAnnotation != null) {
-                if (classAnnotation.ignore()) {
-                    return joinPoint.proceed();
-                }
-                classAndMethod.append(classAnnotation.value()).append("-");
-            }
-
-            if (methodAnnotation != null) {
-                if (methodAnnotation.ignore()) {
-                    return joinPoint.proceed();
-                }
-                classAndMethod.append(methodAnnotation.value());
-            }
-
-            String target = targetClass.getName() + "#" + method.getName();
-            String params = JSONObject.toJSONStringWithDateFormat(joinPoint.getArgs(), dateFormat, SerializerFeature.WriteMapNullValue);
-
-            log.info(STRING_START + "{} 开始调用--> {} 参数:{}", classAndMethod.toString(), target, params);
-
-            long start = System.currentTimeMillis();
-            Object result = joinPoint.proceed();
-            long timeConsuming = System.currentTimeMillis() - start;
-
-            log.info("\n{} 调用结束<-- {} 返回值:{} 耗时:{}ms" + STRING_END, classAndMethod.toString(), target, JSONObject.toJSONStringWithDateFormat(result, dateFormat, SerializerFeature.WriteMapNullValue), timeConsuming);
-            return result;
-        } catch (Throwable throwable) {
-            log.error(throwable.getMessage(), throwable);
+            classAndMethod.append(classAnnotation.value()).append("-");
         }
-        return null;
-    }
 
+        if (methodAnnotation != null) {
+            if (methodAnnotation.ignore()) {
+                return joinPoint.proceed();
+            }
+            classAndMethod.append(methodAnnotation.value());
+        }
+
+        String target = targetClass.getName() + "#" + method.getName();
+
+        String params = null;
+             params = JSONObject.toJSONStringWithDateFormat(joinPoint.getArgs(), dateFormat, SerializerFeature.WriteMapNullValue);
+
+        log.info(STRING_START + "{} 开始调用--> {} 参数:{}", classAndMethod.toString(), target, params);
+
+        long start = System.currentTimeMillis();
+        Object result = joinPoint.proceed();
+        long timeConsuming = System.currentTimeMillis() - start;
+
+        log.info("\n{} 调用结束<-- {} 返回值:{} 耗时:{}ms" + STRING_END, classAndMethod.toString(), target, JSONObject.toJSONStringWithDateFormat(result, dateFormat, SerializerFeature.WriteMapNullValue), timeConsuming);
+        return result;
+    }
 }
