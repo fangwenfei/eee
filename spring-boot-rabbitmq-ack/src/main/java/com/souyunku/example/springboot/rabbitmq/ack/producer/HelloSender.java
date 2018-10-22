9ddae510c8970c43a76342d73f8ebdb754f99207
yanpenglei <yanpenglei>
2018-10-22 16:24:45 +0800

    ack

--- spring-boot-rabbitmq-ack/src/main/java/com/souyunku/example/springboot/rabbitmq/ack/producer/HelloSender.java
+++ spring-boot-rabbitmq-ack/src/main/java/com/souyunku/example/springboot/rabbitmq/ack/producer/HelloSender.java
@@ -1,51 +1,70 @@
 package com.souyunku.example.springboot.rabbitmq.ack.producer;
 
+import com.souyunku.example.springboot.rabbitmq.ack.receiver.HelloReceiver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.rabbit.support.CorrelationData;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
 
 @Component
-public class HelloSender implements RabbitTemplate.ReturnCallback {
+public class HelloSender implements RabbitTemplate.ReturnCallback, RabbitTemplate.ConfirmCallback {
+
+    private Logger logger = LoggerFactory.getLogger(HelloSender.class);
 
     @Autowired
     private RabbitTemplate rabbitTemplate;
 
     public void sendMessage(String context) {
-        System.out.println("HelloReceiver发送内容: " + context + "，发送时间:" + new Date());
-
 
         // 消息发送失败返回到队列中, application.properties 配置 spring.rabbitmq.publisher-returns=true
         rabbitTemplate.setMandatory(true);
 
         this.rabbitTemplate.setReturnCallback(this);
+        this.rabbitTemplate.setConfirmCallback(this);
 
         this.rabbitTemplate.setConfirmCallback((correlationData, ack, cause) -> {
             if (!ack) {
-                System.out.println("sendMessage 发送失败" + cause + correlationData.toString());
+                logger.info("HelloSender 发送失败：" + cause + correlationData.toString());
             } else {
-                System.out.println("sendMessage 发送成功 ");
+                logger.info("HelloSender 发送成功");
             }
         });
+
+        logger.info("HelloSender 发送的消息内容：{}", context);
+
         this.rabbitTemplate.convertAndSend("hello", context);
+
     }
 
 
     /**
      * 失败后返回消息回调
-     *
-     * @param message    消息返回的消息
-     * @param replyCode  回复代码
-     * @param replyText  回复文本
-     * @param exchange   交换交换
-     * @param routingKey 路由密钥
+     * <p>
+     * 当消息发送出去找不到对应路由队列时，将会把消息退回
+     * 如果有任何一个路由队列接收投递消息成功，则不会退回消息
      */
     @Override
     public void returnedMessage(Message message, int replyCode, String replyText, String exchange, String routingKey) {
-        System.out.println("return--message:" + new String(message.getBody()) + ",replyCode:" + replyCode + ",replyText:" + replyText + ",exchange:" + exchange + ",routingKey:" + routingKey);
+        logger.info("return--message:" + new String(message.getBody()) + ",replyCode:" + replyCode + ",replyText:" + replyText + ",exchange:" + exchange + ",routingKey:" + routingKey);
     }
+
+    /**
+     * 实现ConfirmCallback
+     * <p>
+     * ACK=true仅仅标示消息已被Broker接收到，并不表示已成功投放至消息队列中
+     * ACK=false标示消息由于Broker处理错误，消息并未处理成功
+     */
+    @Override
+    public void confirm(CorrelationData correlationData, boolean ack, String cause) {
+        logger.info("消息id: " + correlationData + "确认" + (ack ? "成功:" : "失败"));
+    }
+
 
 
 }
